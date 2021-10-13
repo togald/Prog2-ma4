57cdfd9de2a61265a01d04882638e26f73733807
Togald Nilsson <togald.nilsson@gmail.com>
2021-10-13 13:09:51 +0200

    Added plot and finished

--- MA4_1.py
+++ MA4_1.py
@@ -8,6 +8,7 @@ import os
 import functools
 import concurrent.futures as future
 from time import perf_counter as pc
+from multiprocessing import cpu_count
 
 class MCC:
     def __init__(self, n=10):
@@ -126,24 +127,86 @@ def main_1_2():
         print(f"Approximated volume: {mchs.mcvol()}")
         print(f"Exact volume       : {mchs.anvol()}")
 
+def hsvol(dim):
+    """
+        Calculates the volume of a hypersphere of dimension dim and radius 1 analytically
+    """
+    return math.pi ** (dim / 2) / math.gamma( dim / 2 + 1 )
+
+def hsvol_mc_mt(threads, n=1000, dim=11):
+    """
+        Multi-threaded calculation of the volume of a hypersphere using monte-carlo methods
+    """
+    res = 0
+    n_threads = list(map( lambda x: int(n/threads), list(range(threads)) ))
+    if n%threads != 0: 
+        n_threads[len(n_threads)-1] += n % threads
+    tasks   = list(range( threads ))
+    results = []
+    with future.ProcessPoolExecutor() as ex:
+        results_map = ex.map(_hsvol_mc_mt, n_threads)
+        for result in results_map: results.append(result)
+    return sum(results)/len(results)
+
+def _hsvol_mc_mt(n=1000, dim=11):
+    mchs = MCHS(n, dim)
+    return mchs.mcvol()
+        
+
 def main_1_3():
+    n = 100000
+    n = 10000000 # This is the desired number
     start = pc()
-    mchs = MCHS(100000, 11)
-    stop = pc()
+    mchs = MCHS(n, 11)
     print("--- Single-thread execution ---")
     print(f"Hypersphere in {mchs.dim} dimensions, n = {mchs.num}")
     print(f"Approximated volume: {mchs.mcvol()}")
-    print(f"Exact volume       : {mchs.anvol()}")
+    print(f"Exact volume       : {hsvol(mchs.dim)}")
+    stop = pc()
     print(f"Execution time     : {stop-start} seconds")
+    print()
+    
+    threads = 10
+    start = pc()
+    # Important difference: multithreading is sent to a separate function, since population of the random MCHS object takes significant time for large values of n.     
+    print(f"--- {threads}-thread execution ---")
+    print(f"Hypersphere in 11 dimensions, n = {n}")
+    print(f"Approximated volume: {hsvol_mc_mt(threads, n)}")
+    print(f"Exact volume       : {hsvol(11)}")
+    stop = pc()
+    print(f"Execution time     : {stop-start} seconds")
+    print()
+    
+    # n = 1000000
+    print(f"--- How many threads are sensible, really? ---")
+    print(f"This system runs on {cpu_count()} processor cores. Are more threads than cores beneficial?")
+    print(f"Hypersphere in 11 dimensions, n = {n}")
+    times = []
+    for threads in range( 1, cpu_count() + int(cpu_count()/2) + 1 ):
+        print(f"Threads  : {threads}")
+        start = pc()
+        hsvol_mc_mt(threads, n)
+        time = pc() - start
+        times.append(time)
+        print(f"Exec time: {time}")
+    fig = plt.figure(figsize=(8,8), dpi=144)
+    plt.plot( range(1, len(times)+1)
+            , times
+            )
+    plt.title(f"Execution time depending on number of threads")
+    plt.xlabel(f"Number of threads, system has {cpu_count()} cores")
+    plt.ylabel("Execution time, seconds")
+    fig.tight_layout()
+    fig.savefig(f"Exectimes_n={n}_cores={cpu_count()}.png")
     
 
 if __name__ == '__main__':
     print("Hello world!")
     print("MA4 1.1: First, let's do some pi-approximations!")
-    #main_1_1()
+    main_1_1()
     print()
     print("MA4 1.2: Now, some hyperspheres! This is where math goes weird.")
-    #main_1_2()
+    main_1_2()
     print()
     print("MA4 1.3: Time for parallel programming, useful on hyperspheres!")
     main_1_3()
